227aa71036874cabdb51e7cd524babbfcfb352ef
Ryan Plessner <plessner.ryan@gmail.com>
2013-12-06 08:54:24 -0500

    removed tabs in main file. dammit notepad++

--- alive.js
+++ alive.js
@@ -1,42 +1,42 @@
 var fs = require('fs'),
-	http = require('http'),
+    http = require('http'),
     express = require('express'),
-	_ = require('underscore'),
-	schedule = require('node-schedule'),
-	Handlebars = require('handlebars'),
-	app = express(),
-	templateFile = 'healthcheck.hbs';
+    _ = require('underscore'),
+    schedule = require('node-schedule'),
+    Handlebars = require('handlebars'),
+    app = express(),
+    templateFile = 'healthcheck.hbs';
 	
 var helpers = {
-	template: Handlebars.compile(fs.readFileSync(templateFile).toString()),
-	updateStatus: function(sites, status) {
-		_.each(sites, function(site) {
-			var request = http.get(site, function(response) {
-				status[site] = response.statusCode;
-			});
-			request.on('error', function(err) {
-				status[site] = err.statusCode;
-			});
-		});
-	}
+    template: Handlebars.compile(fs.readFileSync(templateFile).toString()),
+    updateStatus: function(sites, status) {
+        _.each(sites, function(site) {
+            var request = http.get(site, function(response) {
+                status[site] = response.statusCode;
+            });
+            request.on('error', function(err) {
+                status[site] = err.statusCode;
+            });
+        });
+    }
 };
 
 Handlebars.registerHelper('statusStyle', function(status) {
-	return (status >= 400) ? 'bad' : 'good';
+    return (status >= 400) ? 'bad' : 'good';
 });
 	
 exports.launchAliveChecker = function(sites, options) {
-	var status = {};
-	helpers.updateStatus(sites, status);
+    var status = {};
+    helpers.updateStatus(sites, status);
 	
-	schedule.scheduleJob('30 * * * *', function() {
-		helpers.updateStatus(sites, status);
-	});
+    schedule.scheduleJob('30 * * * *', function() {
+        helpers.updateStatus(sites, status);
+    });
 	
-	app.use('/css', express.static(__dirname +  '/css'));
+    app.use('/css', express.static(__dirname +  '/css'));
 	
-	app.get('/', function(request, response) {
-		response.send(helpers.template(status));
-	});
-	app.listen(options.port);
+    app.get('/', function(request, response) {
+        response.send(helpers.template(status));
+    });
+    app.listen(options.port);
 };
